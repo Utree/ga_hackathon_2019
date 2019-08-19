6d026402fee92a02c86829d29d676aac2c645e4a
Utree <yuukis322+github@gmail.com>
2019-08-19 18:12:33 +0900

    Alamofireでデータを取得しjsonをパース

--- chikaba-app/ViewController.swift
+++ chikaba-app/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import Alamofire
+
+struct Item: Codable {
+    let id: Int
+    let prices_id: Int
+    let vacants_id: Int
+    let name: String
+    let category: String
+    let discription: String
+    let main_image_path: String
+    let place: String
+    let thumbnail_path: String
+    let webpage: String
+    let latitude: Float
+    let longitude: Float
+    let price: Price
+    let vacant: Vacant
+    
+    struct Price: Codable {
+        let id: Int
+        let name: String
+    }
+    struct Vacant: Codable {
+        let id: Int
+        let name: String
+    }
+}
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // データの取得
+        _ = requestFirst()
     }
-
-
+    
+    
+    private func requestFirst() -> [Item]? {
+        var result: [Item]? = nil
+        
+//        リクエスト
+        Alamofire.request("https://api.myjson.com/bins/19ho7f").response { response in
+            let decoder = JSONDecoder()
+            
+            guard let data = response.data else { return }
+//            Jsonのパース
+            do {
+                result = try decoder.decode([Item].self, from: data)
+            } catch {
+                print(error)
+            }
+        }
+        return result
+    }
 }
 
