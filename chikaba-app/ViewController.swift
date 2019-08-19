7b02c321bb29f6a4c4de70d47727f86b1159e15d
N-Raito <b1017013@gmail.com>
2019-08-19 13:53:00 +0900

    Initial Commit

---
+++ chikaba-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chikaba-app
+//
+//  Created by 西俣雷斗 on 2019/08/19.
+//  Copyright © 2019 西俣雷斗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
