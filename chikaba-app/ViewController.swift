72a424e88646f1667fa404ac811d2186dd269fd7
N-Raito <b1017013@gmail.com>
2019-08-19 18:34:47 +0900

    ストーリーボードとソースコードも手を加えちゃった

--- chikaba-app/ViewController.swift
+++ chikaba-app/ViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate
+    ,UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    @IBOutlet weak var TableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+  
+    @IBAction func JumpSearchScreen(_ sender: Any) {
+    }
 }
 
