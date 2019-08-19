e302e920236ac7793f9eb2897916aa0f1528cf8d
N-Raito <b1017013@gmail.com>
2019-08-20 02:57:11 +0900

    ホーム画面から詳細画面に遷移する際にデータを受け渡し

---
+++ chikaba-app/ShopInfomation.swift
@@ -0,0 +1,28 @@
+//
+//  ShopInfomation.swift
+//  chikaba-app
+//
+//  Created by 西俣雷斗 on 2019/08/20.
+//  Copyright © 2019 西俣雷斗. All rights reserved.
+//
+
+import UIKit
+
+class ShopInfomation: UIViewController {
+
+    var Name = ""
+    var Image = ""
+    var url = ""
+    @IBOutlet weak var shopImage: UIImageView!
+    
+    @IBOutlet weak var shopName: UILabel!
+   
+    @IBOutlet weak var URL: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        shopName.text = Name
+        URL.setTitle(url, for: .normal)
+    }
+    
+}
