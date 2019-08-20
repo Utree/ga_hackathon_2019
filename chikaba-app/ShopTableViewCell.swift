ec8d143b884a5165d10933c6b4408bddb9ef70f0
N-Raito <b1017013@gmail.com>
2019-08-20 15:32:35 +0900

    ラスト多分

--- chikaba-app/ShopTableViewCell.swift
+++ chikaba-app/ShopTableViewCell.swift
@@ -26,14 +26,14 @@ class ShopTableViewCell: UITableViewCell {
     }
     
     func setupCell(with viewModel: ItemViewModel) {
-        print(viewModel.vacantID)
-        if viewModel.vacantID == 4 { self.vacantColor.backgroundColor = UIColor.init(red: 0, green: 100, blue: 0, alpha: 100)
+        
+        if viewModel.vacantID == 4 { self.vacantColor.backgroundColor = UIColor.init(red: 2/255, green: 207/255, blue: 40/255, alpha: 1)
         }
         if viewModel.vacantID == 5 {
-            self.vacantColor.backgroundColor = UIColor.init(red: 100, green: 100, blue: 0, alpha: 100)
+            self.vacantColor.backgroundColor = UIColor.init(red: 252/255, green: 215/255, blue: 3/255, alpha: 1)
         }
         if viewModel.vacantID == 6 {
-            self.vacantColor.backgroundColor = UIColor.init(red: 100, green: 0, blue: 0, alpha: 100)
+            self.vacantColor.backgroundColor = UIColor.init(red: 252/255, green: 98/255, blue: 3/255, alpha: 1)
         }
         
         
